d864bc6da75df9b7c9cab834b21c5796744d346a
Dhruv Sahu <dhruvsahu2000@gmail.com>
2022-06-13 21:09:42 +0530

    Create btreeinorder_94.java

---
+++ Java/btreeinorder_94.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode temp = root;
+        while(!stack.isEmpty() || temp!=null){
+            if(temp!=null){
+                stack.push(temp);
+                temp=temp.left;
+            }
+            else{
+                temp =stack.pop();
+                list.add(temp.val);
+                temp = temp.right;
+            }
+        }
+        return list;
+    }
+}
