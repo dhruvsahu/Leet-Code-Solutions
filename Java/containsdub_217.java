3efa3ee5d7e073962d822bb220d10def033b634f
Dhruv Sahu <dhruvsahu2000@gmail.com>
2022-06-21 21:25:16 +0530

    Create containsdub_217.java

---
+++ Java/containsdub_217.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(!set.add(nums[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+}
