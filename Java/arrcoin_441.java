e65e71db638f5dffe9b81de2b5835ea1cb113e7d
Dhruv Sahu <dhruvsahu2000@gmail.com>
2022-07-08 19:03:03 +0530

    Update arrcoin_441.java

--- Java/arrcoin_441.java
+++ Java/arrcoin_441.java
@@ -8,6 +8,21 @@ public class Solution {
         }
         return (int)i-2;
     }
-}class arrcoin_441 {
-    
+}class Solution {
+    public int arrangeCoins(int n) {
+        int i = 1;
+        int count = 0 ;
+        if(n==1)
+        {
+            return 1;
+        }
+        while(i<=n)
+        {
+            n = n-i;
+            count++;
+            i++;
+        }
+        return count;
+        
+    }
 }
