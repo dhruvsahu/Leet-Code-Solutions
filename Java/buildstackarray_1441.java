652bbf74018d81765e5b5ae671c016eb36115676
Dhruv Sahu <dhruvsahu2000@gmail.com>
2022-07-22 00:52:05 +0530

    Create buildstackarray_1441.java

---
+++ Java/buildstackarray_1441.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> list=new ArrayList<>();
+        int i=1;
+        int j=0;
+        while(j<target.length){
+            if(target[j]==i){
+                list.add("Push");
+                j++;
+            }else{
+                list.add("Push");
+                list.add("Pop");
+            }
+            i++;
+        }
+        return list;
+    }
+}
