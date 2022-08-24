a6cf4ab1567981c095f3586f2c80c57c3fe992ae
Dhruv Sahu <dhruvsahu2000@gmail.com>
2022-08-24 14:42:39 +0530

    Create decodeXOR_1720.java

---
+++ Java/decodeXOR_1720.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] decode(int[] encoded, int first) {
+        int[] arr=new int[encoded.length+1];
+        arr[0]=first;
+        for(int i=0;i<encoded.length;i++){
+            int j=encoded[i]^arr[i];
+            arr[i+1]=j;
+        }
+        return arr;
+    }
+}
